81fee51cad5012b473bbabe9804baf3706f31c67
Md Munna Mia <mdmunnamia987@gmail.com>
2024-12-05 19:26:08 +0600

    all the handler function

--- handlers.go
+++ handlers.go
@@ -1,1 +1,9 @@
 package main
+
+import "net/http"
+
+func (app *application) add(w http.ResponseWriter, r *http.Request)      {}
+func (app *application) subtract(w http.ResponseWriter, r *http.Request) {}
+func (app *application) multiply(w http.ResponseWriter, r *http.Request) {}
+func (app *application) divide(w http.ResponseWriter, r *http.Request)   {}
+func (app *application) sum(w http.ResponseWriter, r *http.Request)      {}
